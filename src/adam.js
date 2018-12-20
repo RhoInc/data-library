22d00bf02132de7570a2835197de86cc76ab95b7
samussiah <samussiah@gmail.com>
2018-12-20 17:05:29 -0500

    git add

---
+++ src/adam.js
@@ -0,0 +1,38 @@
+const explorer = webcodebook.createExplorer(
+    '#container',
+    {
+        files: [
+            {
+                name: 'ADSL',
+                path: './adsl.csv',
+                rows: 150,
+                columns: 10,
+            },
+            {
+                name: 'ADAE',
+                path: './adae.csv',
+                rows: 150,
+                columns: 10,
+            },
+            {
+                name: 'ADCM',
+                path: './adcm.csv',
+                rows: 150,
+                columns: 10,
+            },
+            {
+                name: 'ADVS',
+                path: './advs.csv',
+                rows: 150,
+                columns: 10,
+            },
+            {
+                name: 'ADLB',
+                path: './adlb.csv',
+                rows: 150,
+                columns: 10,
+            },
+        ]
+    }
+);
+explorer.init();

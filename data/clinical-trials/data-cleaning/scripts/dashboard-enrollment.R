c697d0299cbe3d1d76372df18e0f2c8ee2a9257b
Preston Burns <Preston_Burns@rhoworld.com>
2019-07-25 17:12:18 -0400

    start dashboard enrollment data script

---
+++ data/clinical-trials/data-cleaning/scripts/dashboard-enrollment.R
@@ -0,0 +1,44 @@
+
+library(tidyverse)
+
+sites <- c("Clinical Site 1","Clinical Site 2","Clinical Site 3","Clinical Site 4","Clinical Site 5")
+
+site_values <- sample(sites, 80, replace=TRUE, prob=c(0.12, 0.12, 0.16, .3, .3)) 
+
+site_short_values <- substr(site_values,10,15)
+
+data <- data.frame(site=site_values, site_short = site_short_values)
+
+# Add subjid
+for (i in 1:nlevels(data$site)) {
+  
+  site_level <- levels(data$site)[i]
+  subj_count = 0
+  for(j in 1:nrow(data[data$site == site_level,])){
+    print (j)
+     
+    if (j %% 2 == 0)  {
+      
+      subj_count = subj_count + 1
+    } else if (rbinom(n=1, size=1, prob=0.8) == c(1)) {
+      subj_count = subj_count + 1
+    }
+      print(subj_count)
+      data[data$site == site_level,"subjid"][j] <- paste0("0",i,"-00",subj_count)
+    }
+      
+      }
+
+data$population <- "Screened"
+
+data$population_superset <- ""
+
+data[duplicated(data$subjid),"population"] <- "Randomized"
+
+data[duplicated(data$subjid),"population_superset"] <- "Screened"
+
+# I wanted to see how far I could get without tidyverse but I throw in the towel here
+
+data <- data %>%
+  mutate(population_order = ifelse(population == "Screened", 1, 2) ) %>%
+  mutate(population_color = ifelse(population == "Screened", "#a6bddb", "#3690c0") )
